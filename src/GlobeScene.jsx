ee2c8b13fb7660d58b139e56b74846dd47a4b9c1
k-boateng <kwameboateng.kb@proton.me>
2025-08-25 21:10:21 -0400

    Globe Visualization
    Added starry background

--- src/GlobeScene.jsx
+++ src/GlobeScene.jsx
@@ -54,5 +54,61 @@ export default function GlobeScene() {
         .catch((e) => console.error("GeoJSON load error:", e));
 
         
+        const starObjs = [];
+        (function starBackground() {
+        const starCount = 4000;
+        const minRadius = controls.maxDistance + 0.5;
+        const maxRadius = camera.far * 0.95;
+
+        const texturePaths = [
+            "/trans_star.png",
+            "/trans_star2.png",
+            "/trans_star3.png",
+            "/trans_star4.png",
+        ];
+        const loader = new THREE.TextureLoader();
+        const textures = texturePaths.map((p) => loader.load(p));
+
+        const buckets = textures.map(() => []);
+
+        for (let i = 0; i < starCount; i++) {
+            const u = Math.random(),
+            v = Math.random();
+            const theta = 2 * Math.PI * u;
+            const phi = Math.acos(2 * v - 1);
+
+            const t = Math.random();
+            const r = Math.cbrt(minRadius ** 3 + t * (maxRadius ** 3 - minRadius ** 3));
+
+            const x = r * Math.sin(phi) * Math.cos(theta);
+            const y = r * Math.sin(phi) * Math.sin(theta);
+            const z = r * Math.cos(phi);
+
+            const idx = (Math.random() * textures.length) | 0;
+            buckets[idx].push(x, y, z);
+        }
+
+        buckets.forEach((positions, i) => {
+            if (!positions.length) return;
+
+            const geom = new THREE.BufferGeometry();
+            geom.setAttribute("position", new THREE.Float32BufferAttribute(positions, 3));
+
+            const mat = new THREE.PointsMaterial({
+            map: textures[i],
+            color: 0xffffff,
+            size: 1.0,
+            sizeAttenuation: true,
+            transparent: true,
+            alphaTest: 0.3,
+            depthWrite: false,
+            blending: THREE.NormalBlending,
+            });
+
+            const stars = new THREE.Points(geom, mat);
+            scene.add(stars);
+            starObjs.push({ geom, mat, stars, tex: textures[i] });
+        });
+        })();
 
 
