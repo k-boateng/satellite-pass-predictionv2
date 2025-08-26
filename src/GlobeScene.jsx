4bc3f8b342f0dd355a31d4764e046b9a5074801b
k-boateng <kwameboateng.kb@proton.me>
2025-08-25 21:02:38 -0400

    Helper file
    Helper file to draw the country lines on the globe

--- src/GlobeScene.jsx
+++ src/GlobeScene.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { drawThreeGeo } from "./threeGeoJSON.js";
+
 
 
