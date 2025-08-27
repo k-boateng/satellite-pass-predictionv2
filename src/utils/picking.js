59f8d45b8ffcc8930769678611905898831604f7
k-boateng <kwameboateng.kb@proton.me>
2025-08-27 18:48:52 -0400

    Refactored satellite selection functionality

---
+++ src/utils/picking.js
@@ -0,0 +1,16 @@
+import * as THREE from "three";
+
+export function eventToNDC(dom, e) {
+  const rect = dom.getBoundingClientRect();
+  return new THREE.Vector2(
+    ((e.clientX - rect.left) / rect.width) * 2 - 1,
+    -((e.clientY - rect.top) / rect.height) * 2 + 1
+  );
+}
+
+export function findSatDot(object) {
+  let o = object;
+  while (o && !o.userData?.satRef) o = o.parent;
+  return o?.userData?.satRef ?? null; // returns SatelliteDot or null
+}
+
